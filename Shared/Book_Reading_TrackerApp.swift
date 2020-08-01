d8f80b89eb8a70663d03d5882be20e94c97b0b72
Chaitanya <74nyjqk+5pbxnaf@kchaitanya.com>
2020-07-31 20:06:42 -0700

    Initial Commit

---
+++ Shared/Book_Reading_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_Reading_TrackerApp.swift
+//  Shared
+//
+//  Created by Chaitanya K Kamatham on 7/31/20.
+//
+
+import SwiftUI
+
+@main
+struct Book_Reading_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
